6974ca57754b6eb4ce348fc6358d443f7ccb9bd4
sunny-1007 <133908769+sunny-1007@users.noreply.github.com>
2025-12-15 23:46:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('Кнопка нажата! Сайт работает!');
+    console.log('Кнопка сработала - JavaScript работает');
+});
